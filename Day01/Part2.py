9dbde30fffa0d29b93673d09c6142b383c84e455
PickleyPickle <214704869+pickleypickle@users.noreply.github.com>
2025-06-20 18:47:23 +0100

    Create Day1 Part2 - Similarity Calculator

--- Day01/Part2.py
+++ Day01/Part2.py
@@ -12,3 +12,28 @@ def read_csv(filename):
             list1.append(row[0][0])
             list2.append(row[0][1])
     return list1, list2
+
+# Calculate similarity scores for each number in list 1 (num*appearances in list 2)
+def calculate_similarity(list1, list2):
+    similarity_list = []
+
+    for num in list1:
+        appearances = list2.count(num)
+        similarity_list.append(int(num) * appearances)
+
+    return similarity_list
+
+def main():
+    filename = 'Day01/numbers.csv'
+    list1, list2 = read_csv(filename)
+    
+    # Calculate the similarity scores
+    similarity_scores = calculate_similarity(list1, list2)
+
+    # Printing results
+    print("Sum of similarity scores:", sum(similarity_scores))
+
+if __name__ == '__main__':
+    main()
+
+# This code reads a CSV file containing two lists of numbers, calculates the similarity scores based on the number of appearances in the second list, and prints the sum of these scores.
